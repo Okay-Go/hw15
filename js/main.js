53d204213068b4b59124031b8640c18cf3d30499
Okay-Go <clushclush@gmail.com>
2015-04-07 17:11:00 +0300

    Homework
    number 15

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(function ($) {
+    $(document).ready(function(){
+        $(".nav a").click(function(){
+            $(".nav a").removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+});
